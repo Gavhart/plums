1aa489b4f99297409fec897bac8d7dfb06909f11
gavhart <har19076@byui.edu>
2024-01-23 15:50:54 -0700

    init commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# PlumsApp
-PlumsApp is a simple App that allows users to create, read, update, and delete (CRUD) notes. It is built using React. The application is hosted on GitHub Pages and can be accessed [here](https://plumsapp.github.io/).
---
+++ plums/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}', // Note the addition of the `app` directory.
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+ 
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
