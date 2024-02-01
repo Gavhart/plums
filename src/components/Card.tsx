691d891ee0a8ed032df491b3731c3503c6693f9d
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-01-31 20:44:49 -0700

    Custom pallete added to tailwind
    - Removed layout=responsive

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({image}:CardProps) {
     <>
       <div className="w-40 h-40 bg-gray-100 mx-auto">
           {image && (
-              <Image src={image} alt="" width={100} height={100} layout="responsive" />
+              <Image src={image} alt="" width={100} height={100} />
           )}
       </div>
     </>
