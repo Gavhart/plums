f8b3a40d75875ed370ee38b5d961d8e1a4a4bca3
gavhart <har19076@byui.edu>
2024-02-06 15:52:17 -0700

    started on header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white p-4">
+            <p className="text-center">© 2024 plums. All rights reserved.</p>
+        </footer>
+    );
+}
