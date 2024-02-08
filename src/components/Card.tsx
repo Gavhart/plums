5e5513c5880ecc6db24ade0f2c68bb48ce80b153
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-02-07 21:04:18 -0700

    PLUM-20 Make and read json data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,14 @@
-import Image from "next/image"
+import Image from "next/image";
 
 interface CardProps {
-    image?: string
+  image?: string;
 }
 
-export default function Card({image}:CardProps) {
+export default function Card({ image }: CardProps) {
   return (
     <>
       <div className="w-40 h-40 bg-gray-100 mx-auto">
-          {image && (
-              <Image src={image} alt="" width={100} height={100} />
-          )}
+        {image && <Image src={image} alt="" width={100} height={100} />}
       </div>
     </>
   );
