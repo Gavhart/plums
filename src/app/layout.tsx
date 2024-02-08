85600403233453462e48c57846efe1d95ccca5c7
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-02-08 16:01:42 -0700

    Put footer in header lol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header></header>
+        <header>
+          <Footer />
+        </header>
         <main>{children}</main>
         <footer></footer>
       </body>
