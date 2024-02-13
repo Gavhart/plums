0aee07465f06df1415ba2089e8a13a01a08a588e
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-02-13 15:48:32 -0700

    Reworked data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,16 +3,17 @@ import projectData from "@/data/data.json";
 export default function TasksPage() {
   return (
     <>
-      {projectData.projects.map((project) => (
-        <div key={project.id} className="border py-4 px-4 flex flex-col gap-4">
-          <p>{project.name}</p>
-          <p>{project.description}</p>
-          {project.tasks.map((task) => (
-            <div key={task.id} className="flex flex-col gap-4 pl-4">
-              <p>{task.title}</p>
-              <p>{task.description}</p>
-              <p>{task.status}</p>
-              <p>{task.dueDate}</p>
+      {projectData.folders.map((folder) => (
+        <div
+          key={folder.id}
+          className="border py-4 px-4 flex flex-col gap-4 cursor-pointer"
+        >
+          <p>{folder.name}</p>
+          {/*  Display notes inside the folder */}
+          {folder.notes.map((note) => (
+            <div key={note.id}>
+              <p>{note.title}</p>
+              <p>{note.content}</p>
             </div>
           ))}
         </div>
