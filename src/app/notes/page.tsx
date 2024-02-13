65801b5cb2ca36cb3d0ef58ed8a87846a6e406b6
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-02-13 16:08:11 -0700

    Dynamic routes

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,21 +1,21 @@
 import projectData from "@/data/data.json";
+import Link from "next/link";
 
 export default function TasksPage() {
   return (
     <>
       {projectData.folders.map((folder) => (
-        <div
-          key={folder.id}
-          className="border py-4 px-4 flex flex-col gap-4 cursor-pointer"
-        >
-          <p>{folder.name}</p>
-          {/*  Display notes inside the folder */}
+        <div key={folder.id} className="border py-4 px-4 flex flex-col gap-4">
+          <Link href={`notes/${folder.id}`}>
+            <p>{folder.name}</p>
+          </Link>
+          {/* todo Displau this data when clicking this data
           {folder.notes.map((note) => (
             <div key={note.id}>
               <p>{note.title}</p>
               <p>{note.content}</p>
             </div>
-          ))}
+          ))}*/}
         </div>
       ))}
     </>
