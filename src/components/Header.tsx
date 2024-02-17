16058710d722b9f5b9d34722f4ff24d7e0b19deb
gavhart <har19076@byui.edu>
2024-02-17 12:39:42 -0700

    added a favicon just dont know how to insert

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className= 'flex justify-between align-middle'>
     <img className='w-24 h-24' src={logo.src} alt="Logo" />
       <h1 className={'flex text-5xl text-center'}>
-        Plums
+      Plums
         </h1>
         <div className="flex gap-4 p-4">
       <MagnifyingGlassIcon className="w-8 h-8" />
