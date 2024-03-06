bfb57bbc7ab6d7108d339691d3a8cb81cea43894
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-06 14:26:42 -0700

    PLUM-33 Fixed linting errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "../assets/plums.webp";
 import { MagnifyingGlassIcon, Bars3Icon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="flex flex-col md:flex-row justify-between items-center px-4 py-2 md:px-8 md:py-4">
       <div className="flex items-center">
-        <img className="w-16 h-16 md:w-24 md:h-24" src={logo.src} alt="Logo" />
+        <Image className="w-16 h-16 md:w-24 md:h-24" alt="logo" src={logo} />
         <h1 className="text-xl md:text-3xl font-bold ml-2">Plums</h1>
       </div>
       <div className="flex items-center mt-4 md:mt-0">
