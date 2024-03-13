839884b99850308897191c43931681bcea06a175
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-12 19:53:05 -0600

    PLUM-12 Fixed linting errors

--- src/components/ImageContentData.tsx
+++ src/components/ImageContentData.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { getImageContent } from "@/lib/queries/imageNotes";
 
 const ImageContentData = async ({ noteId }: { noteId: string }) => {
@@ -8,7 +9,7 @@ const ImageContentData = async ({ noteId }: { noteId: string }) => {
   }
   return (
     <div>
-      <img src={imageContent.url} alt={imageContent.title} />
+      <Image src={imageContent.url} alt={imageContent.title} />
     </div>
   );
 };
