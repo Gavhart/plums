894c0807e7ae2cf4f95d3e400c42e8ee29ec2aac
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-15 10:52:17 -0600

    PLUM-43 Closes when submitting

--- src/components/CreateTextNote.tsx
+++ src/components/CreateTextNote.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import React from "react";
-import { Input, Textarea, Button } from "@material-tailwind/react";
+import React, { useState } from "react";
+import { Input, Textarea, Button, Dialog } from "@material-tailwind/react";
 
 const CreateTextNote = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   return (
     <div className="flex flex-col gap-8 py-4 bg-custom-off-white">
-      <Input name="title" label="Title" crossOrigin={undefined} />
-      <Textarea name="content" label="Your notes" rows={16} />
-      <Button type="submit" placeholder={undefined}>
-        Submit
+      <Button onClick={handleOpen} variant="gradient" placeholder={undefined}>
+        Create Text Note
       </Button>
+      <div hidden={!open} className={`${open && "flex flex-col gap-8 "}`}>
+        <Input name="title" label="Title" crossOrigin={undefined} />
+        <Textarea name="content" label="Your notes" rows={16} />
+        <Button type="submit" placeholder={undefined} onClick={handleOpen}>
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
