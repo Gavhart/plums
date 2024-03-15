de9670a7ee7e3bb71dd1fdab5da9b6e384ea19e9
Abraham Jimenez <abraham.jimenez.romo@gmail.com>
2024-03-15 12:02:35 -0600

    PLUM-44 Optimized imports

--- src/app/topics/[folderId]/page.tsx
+++ src/app/topics/[folderId]/page.tsx
@@ -3,15 +3,7 @@ import TextNotesData from "@/components/TextNotesData";
 import LinkNotesData from "@/components/LinkNotesData";
 import ImageNotesData from "@/components/ImageNotesData";
 import TopicNameBreadCrumb from "@/components/Breadcrumbs/TopicNameBreadCrumb";
-import { getTopicName, updateTopic } from "@/lib/queries/topics";
-import { UpdateTopicName } from "@/components/UpdateTopicName";
-import { redirect } from "next/navigation";
-import CreateTextNote from "@/components/CreateTextNote";
-import { createTextNote } from "@/lib/queries/textNotes";
-import { createLinkNote } from "@/lib/queries/linkNotes";
-import CreateLinkNote from "@/components/CreateLinkNote";
-import { createImageNote } from "@/lib/queries/imageNotes";
-import CreateImageNote from "@/components/CreateImageNote";
+import { getTopicName } from "@/lib/queries/topics";
 import CreateNotes from "@/components/CreateNotes";
 import UpdateTopicNameContainer from "@/components/UpdateTopicNameContainer";
 
